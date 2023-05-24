c65bf22ff584e71820c87882f568cb596c7bbcff
Harold <harmed2002@hotmail.com>
2023-05-24 18:33:09 -0500

    Se incluye la validación de existencia de usuario.

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,10 @@
 var express = require('express');
 var router = express.Router();
 
+var mongoose = require('mongoose');
+var User = require('../models/User.js');
+var db = mongoose.connection;
+
 /* GET users listing. */
 router.get('/', function (req, res, next) {
 	res.send('respond with a resource');
@@ -63,5 +67,61 @@ router.get('/:id', function(req, res) {
 });
 
 
+// GET del listado de usuarios ordenados por fecha de creación
+router.get('/', function (req, res, next) {
+	User.find().sort('-creationdate').exec(function(err, users) {
+		if (err) res.status(500).send(err);
+		else res.status(200).json(users);
+	});
+});
+
+// GET de un único usuario por su Id
+router.get('/:id', function (req, res, next) {
+	User.findById(req.params.id, function (err, userinfo) {
+		if (err) res.status(500).send(err);
+		else res.status(200).json(userinfo);
+	});
+});
+
+// POST de un nuevo usuario
+router.post('/', function (req, res, next) {
+	User.create(req.body, function (err, userinfo) {
+		if (err) res.status(500).send(err);
+		else res.sendStatus(200);
+	});
+});
+
+// PUT de un usuario existente identificado por su Id
+router.put('/:id', function (req, res, next) {
+	User.findByIdAndUpdate(req.params.id, req.body, function (err, userinfo) {
+		if (err) res.status(500).send(err);
+		else res.sendStatus(200);
+	});
+});
+
+router.delete('/:id', function (req, res, next) {
+	User.findByIdAndDelete(req.params.id, function (err, userinfo) {
+		if (err) res.status(500).send(err);
+		else res.sendStatus(200);
+	});
+});
+
+// Comprueba si el usuario existe
+router.post('/signin', function (req, res, next) {
+	User.findOne({username: req.body.username}, function (err, user) {
+		if (err) res.status(500).send('¡Error comprobando el usuario!');
+		// Si el usuario existe...
+		if (user != null){
+			user.comparePassword(req.body.password,function(err, isMatch){
+				if(err) return next(err);
+				// Si el password es correcto...
+				if (isMatch)
+					res.status(200).send({message: 'ok', role: user.role, id: user._id});
+				else
+					res.status(200).send({message: 'ok'});
+			});
+		}else res.status(401).send({message: 'ok'});
+	});
+});
 
 module.exports = router;
